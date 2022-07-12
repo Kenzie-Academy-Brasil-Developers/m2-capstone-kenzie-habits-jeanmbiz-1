ed42b35af2d69e3ab560891c9ca3e42458174954
Jean Michel Biz <jeanmbiz@hotmail.com>
2022-07-12 16:44:27 -0300

    feature/paginaLogin: desenvolvi a pagina de login no index.html, o login.js foi finalizado autorizando o usuário a entrar no site, falta apenas finalizar a parte de css

---
+++ src/controller/login.controller.js
@@ -0,0 +1,25 @@
+
+import RequisicoesUsuario from "./usuario.controller.js";
+
+const botaoLogin = document.querySelector('.celular--botao');
+
+botaoLogin.addEventListener('click', efetuarLogin );
+
+export default efetuarLogin
+async function efetuarLogin(evento){
+    evento.preventDefault();
+
+    const usuario = document.querySelector('.email').value;
+    const senha = document.querySelector('.senha').value;
+
+    const dados = {
+        email: usuario,
+        password: senha
+    }
+
+    const resposta = await RequisicoesUsuario.login(dados)
+    if ('token' in resposta){
+        location.href = 'https://www.youtube.com/'
+    }
+}
+
